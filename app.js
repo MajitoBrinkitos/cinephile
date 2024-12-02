6c9a71a664d4f1241c3723aabd673e14af957058
María-José Mejía <165747579+MajitoBrinkitos@users.noreply.github.com>
2024-12-02 08:56:05 -0800

    Add files via upload
    Files from 2024-12-02

---
+++ app.js
@@ -0,0 +1,10 @@
+ //1.- Sidebar Menu
+ //overlay
+ document.querySelector('.overlay').addEventListener('click', function() {
+
+ })
+
+ //support link
+ document.getElementById("support").onclick = function () {
+     alert("Support Team Link");
+ }
